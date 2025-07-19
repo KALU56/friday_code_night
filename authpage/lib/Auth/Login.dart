05d408ec809439e128e292491bd592b98eaee6aa
kalkidanbehailu55@gmail.com <kalkidanbehailu55@gmail.com>
2025-07-19 11:15:22 +0300

    feat: implement navigation in login

---
+++ authpage/lib/Auth/Login.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import '../Auth//Signup.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome Back!'),
+        
+      ),
+      body: SingleChildScrollView(
+      child:Padding(
+          padding: const EdgeInsets.all(16.0),    
+      child :Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          SizedBox(height: 15,),
+
+          Text('Fill your details or continue with social media'),
+          SizedBox(height: 15,),
+          Text('Email'),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter your email',
+            ),
+            keyboardType: TextInputType.emailAddress,
+          ),
+          SizedBox(height: 20,),
+          Text('Password'),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter your password',
+            ),
+            obscureText: true,
+          ),
+          SizedBox(height: 20,),
+            Row(
+              children: [
+                Checkbox(
+                  value: false,
+                  onChanged: null,
+                ),
+                Text('Remember me'),
+                SizedBox(width: 50,),
+                Text('forget password?'),
+
+              ],
+            ),
+
+          
+          
+          SizedBox(height: 20,),
+          ElevatedButton(
+                      
+            
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue,
+              
+            ),
+              
+          
+            child:Center(
+              child: Text('Login'),
+            ) ),
+
+          
+          SizedBox(height: 20,),
+          
+           Center(
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.g_mobiledata, color: Colors.red),
+                SizedBox(width: 10),
+                Text('Log in with Google'),
+              ],
+            ),
+           ),
+            
+          SizedBox(height: 20,),
+            Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('New User? Create an account'),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const Signup()),
+                      );
+                    },
+                    child: Text('Sign Up'),
+                  ),
+                ],
+              ),
+            )
+                      
+        
+        ],
+
+      )
+    )
+    )
+    );
+  }
+}
---
+++ authpage/lib/Auth/Signup.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+class Signup extends StatelessWidget {
+  const Signup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register Account!'),
+        
+      ),
+      body: SingleChildScrollView(
+        child:Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          SizedBox(height: 15,),
+
+          Text('Fill your details or continue with social media'),
+          SizedBox(height: 15,),
+          Text('Name'),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter your Name',
+            ),
+            keyboardType: TextInputType.emailAddress,
+          ),
+          SizedBox(height: 20,),
+          Text('Email'),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter your email',
+            ),
+            keyboardType: TextInputType.emailAddress,
+          ),
+          SizedBox(height: 20,),
+          Text('Password'),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter your password',
+            ),
+            obscureText: true,
+          ),
+          SizedBox(height: 20,),
+            Row(
+              children: [
+                Checkbox(
+                  value: false,
+                  onChanged: null,
+                ),
+                Text('Remember me'),
+              ],
+            ),
+
+          SizedBox(width: 20,),
+       
+          ElevatedButton(
+                      
+            
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue,
+              
+            ),
+              child:Center(child: Text('Signup'),),),
+          
+            
+          Center(child: Text('or'),),
+         
+          SizedBox(height: 20,),
+                   Center(
+                       child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                   mainAxisSize: MainAxisSize.min,
+                  children: [
+                    
+                    Icon(Icons.g_mobiledata, color: Colors.red),
+                    SizedBox(width: 10),
+                    Text('Log in with GitHub'),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                   mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.g_mobiledata, color: Colors.red),
+                    SizedBox(width: 10),
+                    Text('Log in with Google'),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                   mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.facebook, color: Colors.blue),
+                    SizedBox(width: 10),
+                    Text('Log in with Facebook'),
+                  ],
+                ),
+              ],
+            ),
+                   ) ,
+       
+          
+
+            
+          SizedBox(height: 20,),
+          Text('Already have an account? login'),
+        
+        ],
+        )
+        
+
+        )
+        
+
+      )
+    );
+  }
+}
