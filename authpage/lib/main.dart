5b07b06f6ae4c753c441575647c8a508d89bedc9
kalkidanbehailu55@gmail.com <kalkidanbehailu55@gmail.com>
2025-07-24 17:36:33 +0300

    fix:remove debuger

--- authpage/lib/main.dart
+++ authpage/lib/main.dart
@@ -14,6 +14,7 @@ class MyAuth extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
   
       title: 'Auth Page',
       initialRoute: '/welcome',
