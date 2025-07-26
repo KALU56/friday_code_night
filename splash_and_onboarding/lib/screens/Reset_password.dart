5d384771768eaeffb40ded27841fcad5fe6f241f
kalkidanbehailu55@gmail.com <kalkidanbehailu55@gmail.com>
2025-07-26 16:43:59 +0300

    feat: implement reset password page

--- splash_and_onboarding/lib/screens/Reset_password.dart
+++ splash_and_onboarding/lib/screens/Reset_password.dart
@@ -1,10 +1,96 @@
 import 'package:flutter/material.dart';
+import '../core/assets.dart';
+import './login_siginup.dart';
 
 class ResetPassword extends StatelessWidget {
   const ResetPassword({super.key});
 
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 24),
+              Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(Assets.tag),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 15),
+
+              Text(
+                'Tasktugas',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900,
+                  color: Color.fromARGB(255, 22, 126, 1),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 28),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Enter verification code',
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
+              ),
+              SizedBox(height: 1),
+              Text('you have a code to bimap22@gmail.com'),
+             
+            ],
+          ),
+          SizedBox(height: 45),
+        
+                   
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const LoginSiginup()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 244, 245, 244),
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Center(child: Text('continue')),
+                ),
+                 SizedBox(height: 25),
+                       ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const LoginSiginup()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 244, 245, 244),
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: const Center(child: Text('Cancel')),
+                ),
+        ],
+      ),
+    );
   }
 }
