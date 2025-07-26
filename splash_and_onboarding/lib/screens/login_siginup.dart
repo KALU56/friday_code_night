3b5db2c673984483d99a13cc6c95bd99ddce0ead
kalkidanbehailu55@gmail.com <kalkidanbehailu55@gmail.com>
2025-07-26 14:26:16 +0300

    feat:add functionality login and ssign up

--- splash_and_onboarding/lib/screens/login_siginup.dart
+++ splash_and_onboarding/lib/screens/login_siginup.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../core/assets.dart';
+import '../widgt/login.dart';
+import '../widgt/siginup.dart';
 class LoginSiginup extends StatefulWidget {
   const LoginSiginup({super.key});
 
@@ -8,7 +10,7 @@ class LoginSiginup extends StatefulWidget {
 }
 
 class _LoginSiginupState extends State<LoginSiginup> {
-    bool _obscurePassword = true;
+    
     bool isLogin = true;
     
 
@@ -111,7 +113,7 @@ class _LoginSiginupState extends State<LoginSiginup> {
                 Expanded(
                   child: Container(
                     color: const Color.fromARGB(255, 196, 180, 180),
-                    child: isLogin ? buildLoginUI() : buildSignupUI(),
+                    child: isLogin ? Login() : Siginup(),
                   ),
                 ),
 
@@ -126,229 +128,4 @@ class _LoginSiginupState extends State<LoginSiginup> {
       )
         );
   }
-}
-Widget buildLoginUI() {
-  return Form(
-    // key: _formKey,
-    child: SingleChildScrollView(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        children: [
-          Container(
-              color: const Color.fromARGB(255, 196, 180, 180),
-              child: Column(
-              children: [
-                SizedBox(height: 9,),
-               ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      elevation: 1,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                  
-                    ),
-                    onPressed: () {
-                     
-                    },
-                    child: Row(
-                 
-                      children: [
-                        Image.asset(
-                          Assets.appleImage,
-                          height: 24,
-                          width: 24,
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          "Login with Apple",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 10), 
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      elevation: 1,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-        
-                    ),
-                    onPressed: () {
-                     
-                    },
-                    child: Row(
-                 
-                      children: [
-                        Image.asset(
-                          Assets.googleImage,
-                          height: 24,
-                          width: 24,
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          "Login with Google",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ],
-                    ),
-                  ),
-                      SizedBox(height: 15),
-                      Text('or with continue with email'),
-                      SizedBox(height: 15),
-
-                  
-                  TextField(
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide(
-                          color: const Color.fromARGB(255, 236, 230, 230),
-                          width: 1.5,
-                        ),
-                      ),
-                      hintText: 'Enter your email',
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                    
-                  ),
-                  SizedBox(height: 20),
-               
-                  TextField(
-                    // obscureText: _obscurePassword,
-                    decoration: InputDecoration(
-                      hintText: 'Enter your password',
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                        borderSide: BorderSide(
-                          color: const Color.fromARGB(255, 236, 230, 230),
-                          width: 1.5,
-                        ),
-                      ),
-                      // suffixIcon: IconButton(
-                      //   icon: Icon(
-                      //     _obscurePassword
-                      //         ? Icons.visibility_off
-                      //         : Icons.visibility,
-                      //   ),
-                      //   onPressed: () {
-                      //     setState(() {
-                      //       _obscurePassword = !_obscurePassword;
-                      //     });
-                      //   },
-                      // ),
-                    ),
-                  
-                  ),
-                  SizedBox(height: 20),
-                  
-    
-              
-
-                      
-                        
-                        Text(
-                          'Forgot Password?',
-                          style: TextStyle(
-                          
-                          ),
-                        ),
-                    
-                    
-                    SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      // if (!_formKey.currentState!.validate()) {
-                      //   ScaffoldMessenger.of(context).showSnackBar(
-                      //     SnackBar(
-                      //       content: Text(
-                      //         "Please check your email and password.",
-                      //       ),
-                      //       backgroundColor: Colors.orange,
-                      //     ),
-                      //   );
-                      //   return;
-                      // }
-                      // Navigator.pushNamed(context, '/home');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 11, 172, 118),
-                      foregroundColor: const Color.fromARGB(255, 248, 245, 245),
-                      minimumSize: const Size(double.infinity, 50),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                    ),
-
-                    child: Center(child: Text('Login')),
-                  ),
-
-                  SizedBox(height: 20),
-               Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      
-
-                      Text('By signing up you agree to our '),
-                      Text(
-                        'Terms of service',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 4), 
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('and '),
-                      Text(
-                        'privacy policy',
-                        style: TextStyle(
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline,
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  
-
-
-              ],
-
-             ),
-             ),
-        ],
-      ),
-    ),
-  );
-}
-Widget buildSignupUI() {
-  return Form(
-    // key: _formKey,
-    child: SingleChildScrollView(
-      padding: EdgeInsets.all(16),
-      child: Column(
-        children: [
-          // Replace with sign up fields
-          TextFormField(
-            decoration: InputDecoration(hintText: 'Enter your name'),
-            validator: (value) => value == null || value.isEmpty
-                ? 'Please enter your name'
-                : null,
-          ),
-          SizedBox(height: 10),
-          // Add email, password, confirm password
-          // Add Signup Button
-          // Terms & Privacy
-        ],
-      ),
-    ),
-  );
 }
