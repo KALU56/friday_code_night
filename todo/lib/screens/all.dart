22dc6088b8832a28f3b658d749a533ef3d71e497
kalkidanbehailu55@gmail.com <kalkidanbehailu55@gmail.com>
2025-07-31 13:19:13 +0300

    feat: add icon button in bottom,gesturedetector

---
+++ todo/lib/screens/all.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+class AllDetal extends StatelessWidget {
+  const AllDetal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('All Task List',style: TextStyle(fontWeight: FontWeight.bold),))
+ 
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25.0),
+                      topRight: Radius.circular(25),
+                    ),
+                  ),
+                  width: double.infinity,
+
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Grocery'),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+
+                                child: Text('7 completed'),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [Text('15 Note'), Icon(Icons.arrow_back)],
+                        ),
+                      ],
+                    ),
+                  ),
+                ), ////#1
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 210, 233, 3),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25.0),
+                      topRight: Radius.circular(25),
+                    ),
+                  ),
+                  width: double.infinity,
+
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Grocery'),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+
+                                child: Text('7 completed'),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [Text('15 Note'), Icon(Icons.arrow_back)],
+                        ),
+                      ],
+                    ),
+                  ),
+                ), /////#2
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 141, 245, 5),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25.0),
+                      topRight: Radius.circular(25),
+                    ),
+                  ),
+                  width: double.infinity,
+
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Grocery'),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+
+                                child: Text('7 completed'),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [Text('15 Note'), Icon(Icons.arrow_back)],
+                        ),
+                      ],
+                    ),
+                  ),
+                ), /////#3
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- todo/lib/screens/all_detal.dart
+++ todo/lib/screens/all_detal.dart
@@ -1,143 +1,54 @@
 import 'package:flutter/material.dart';
 
-class AllDetal extends StatelessWidget {
+class AllDetal extends StatefulWidget {
   const AllDetal({super.key});
 
   @override
+  State<AllDetal> createState() => _AllDetalState();
+}
+
+class _AllDetalState extends State<AllDetal> {
+ 
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('All Task List',style: TextStyle(fontWeight: FontWeight.bold),)
+    return SafeArea(child: Scaffold(
+           appBar: AppBar(
+        title: Center(child: Text('All Task List',style: TextStyle(fontWeight: FontWeight.bold),))
  
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Stack(
-          children: [
-            Column(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(25.0),
-                      topRight: Radius.circular(25),
-                    ),
-                  ),
-                  width: double.infinity,
-
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('Grocery'),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(25),
-                                ),
-
-                                child: Text('7 completed'),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [Text('15 Note'), Icon(Icons.arrow_back)],
-                        ),
-                      ],
-                    ),
-                  ),
-                ), ////#1
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 210, 233, 3),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(25.0),
-                      topRight: Radius.circular(25),
-                    ),
-                  ),
-                  width: double.infinity,
-
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('Grocery'),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(25),
-                                ),
-
-                                child: Text('7 completed'),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [Text('15 Note'), Icon(Icons.arrow_back)],
-                        ),
-                      ],
-                    ),
-                  ),
-                ), /////#2
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 141, 245, 5),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(25.0),
-                      topRight: Radius.circular(25),
-                    ),
-                  ),
-                  width: double.infinity,
-
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('Grocery'),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(25),
-                                ),
-
-                                child: Text('7 completed'),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [Text('15 Note'), Icon(Icons.arrow_back)],
-                        ),
-                      ],
-                    ),
-                  ),
-                ), /////#3
+                GestureDetector(
+                  onTap: (){},
+                  child: Text('Today'),
+                ),
+                 GestureDetector(
+                  onTap: (){},
+                  child: Text('Daily'),
+                ),
+                 GestureDetector(
+                  onTap: (){},
+                  child: Text('Monthly'),
+                ),
+                 GestureDetector(
+                  onTap: (){},
+                  child: Text('weekly'),
+                )
+                
+            
               ],
             ),
-          ],
-        ),
-      ),
+          )
+        ],
+      )
+    )
+    
+    
     );
   }
 }
