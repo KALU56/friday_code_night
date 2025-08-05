be7d6c036b5d3f257046914e33a9bb7e1a2480af
kalkidanbehailu55@gmail.com <kalkidanbehailu55@gmail.com>
2025-08-05 11:15:06 +0300

    feat: add model

--- todo/lib/model/todolist.dart
+++ todo/lib/model/todolist.dart
@@ -1,27 +1,17 @@
 import 'package:flutter/widgets.dart';
 
 class TodolistModel {
- String title;
+  String title;
   ImageProvider image;
- Color backgroundColor;
-    int count;
-   Function() onTap;
+  Color backgroundColor;
+  int count;
+  Function() onTap;
 
-
-
-
-  
-  TodolistModel(
-    {
-      required this.title,
-      required this.image,
-      required this.backgroundColor,
-      required this.count,
-      required this.onTap,
-
-    }
-  )
-
-
-
+  TodolistModel({
+    required this.title,
+    required this.image,
+    required this.backgroundColor,
+    required this.count,
+    required this.onTap,
+  });
 }
